15c1e4cb456571473e4f91cea5a4714857d6676a
Cabrera Gaston <cabreragastonivan31@gmail.com>
2022-04-01 19:35:14 -0300

    feature-stock-table

--- src/pages/stock/index.js
+++ src/pages/stock/index.js
@@ -4,14 +4,32 @@ export default function index({ data }) {
   return !data ? (
     <div>Cargando...</div>
   ) : (
-    <div>
-      {data.map((item) => (
-        <ul>
-          <li className="text-3xl font-bold ">
-            Hay en stock {item.cantidad} {item.tipo}
-          </li>
-        </ul>
-      ))}
+    <div className="flex items-center w-full h-full justify-center">
+      <table className="border-separate border border-slate-300">
+        <thead>
+          <tr>
+            <th className="border border-slate-300 p-4">Producto</th>
+            <th className="border border-slate-300 pl-8 pr-8">Cantidad</th>
+            <th className="border border-slate-300 pl-8 pr-8">Actions</th>
+          </tr>
+        </thead>
+        {data.map((item) => (
+          <tbody>
+            <tr className="">
+              <td className="border border-slate-300 text-center p-4">{item.tipo}</td>
+              <td className="border border-slate-300 text-center">{item.cantidad}</td>
+              <td className="border border-slate-300 text-center">
+                <button className="font-semibold text-white p-1 pl-4 pr-4 ml-8 mr-2 rounded bg-blue-600">
+                  Edit
+                </button>
+                <button className="font-semibold text-white p-1 pl-4 pr-4 mr-8 ml-2 rounded bg-red-600">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
